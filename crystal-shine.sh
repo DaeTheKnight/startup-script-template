dddfdb124196fc8249a7bf8705389dec91c53cb7
DaeTheKnight <142058563+DaeTheKnight@users.noreply.github.com>
2025-04-05 15:06:48 -0400

    Update and rename new-class-template.sh to crystal-shine.sh
    changed the girl and background

--- crystal-shine.sh
+++ crystal-shine.sh
@@ -32,7 +32,7 @@ cat <<EOF > /var/www/html/index.html
     body,h1,h3 {font-family: "Raleway", sans-serif}
     body, html {height: 100%}
     .bgimg {
-      background-image: url('https://www.andbeyond.com/wp-content/uploads/sites/5/giraffe-and-sky-line-nairobi.jpg');
+      background-image: url('https://images.stockcake.com/public/9/1/a/91acde9e-2fe9-4654-bd8a-51b0bd1b158a/crystal-sunset-fantasy-stockcake.jpg');
       min-height: 100%;
       background-position: center;
       background-size: cover;
@@ -56,7 +56,7 @@ cat <<EOF > /var/www/html/index.html
   <div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
     <div class="w3-display-topleft w3-padding-large w3-xlarge"></div>
     <div class="w3-display-middle w3-center">
-      <iframe src="https://giphy.com/embed/jQ4FsavAujbMFbYfDz" width="276" height="480" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/jQ4FsavAujbMFbYfDz">via GIPHY</a></p>
+      <iframe src="https://giphy.com/embed/h9pancyNtLmZ3PAwyW" width="268" height="480" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/black-girl-shining-braids-h9pancyNtLmZ3PAwyW">via GIPHY</a></p>
       </iframe>
       <hr class="w3-border-grey" style="margin:auto;width:40%;margin-top:15px;">
       <h3 class="w3-large w3-center" style="margin-top:15px;">
